3c0184b5afe33df758f84e7272dde7fad091d2b3
Abhay kumar sahu <aks.abhaykumarsahu@gmail.com>
2026-09-17 23:16:20 +0530

    Validated the request by making its Response selective and did some refactoring

--- src/main.rs
+++ src/main.rs
@@ -26,18 +26,23 @@ fn handle_connection(mut stream: TcpStream){
     //so the rust program is basically acting as a server, it listens for incoming connections and when it gets one, it handles it
     //so i need to read the data from the client and send it back to him in a response 
     let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader.lines().map(|result| result.unwrap()).take_while(|line| !line.is_empty()).collect();
+    // let http_request: Vec<_> = buf_reader.lines().map(|result| result.unwrap()).take_while(|line| !line.is_empty()).collect();
+
+    let request_line = buf_reader.lines().next().unwrap().unwrap(); //here next returns an Option<Result<String, Error>>, was there even a line to begin with next queries then after succedd it goes to Result<> did reading that line succed that's why two unwraps
 
     // println!("Request: {http_request:#?}"); //here we basically have the GET request from the client
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1"{
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else{
+        ("HTTP/1.1 404 Not Found", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
-
+    
 }
-
-
